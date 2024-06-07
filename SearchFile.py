a9a3e5bd919a1af45c2e568174c92dd3745224ce
Rickert <rickert156@rambler.ru>
2024-06-07 19:51:36 +0400

    Search CSV file| Version 2

--- SearchFile.py
+++ SearchFile.py
@@ -3,10 +3,12 @@ import os
 class SearchFiles:
 
 	def searchCSV():
+		file_type = input('Введите тип файла(по умолчанию будет поиск по csv): ')
 		search_files = os.listdir()
 		list_dirs = []
 		for search_file in search_files:
-		    if '.csv' in search_file:list_dirs += [search_file]
+		    if f'.{file_type}' in search_file:list_dirs += [search_file]
+		    if file_type == ' ' or file_type == '': file_type = 'csv'
 
 		num=0
 		for list_dir in list_dirs:
@@ -16,4 +18,4 @@ class SearchFiles:
 		enter_list = int(input('[x] Введите значение: '))
 		enter_list-=1
 		document = (list_dirs[enter_list])
-		print(f'[x] {document}')
+		print(f'\n[x] {document}')
